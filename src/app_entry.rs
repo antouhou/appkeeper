96160a5dd523fd550573ed345523bb7c24113c34
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2026-06-16 12:09:39 -0300

    populate AppEntry with things

--- src/app_entry.rs
+++ src/app_entry.rs
@@ -1,1 +1,30 @@
-pub struct AppEntry {}
+use std::path::PathBuf;
+
+pub struct AppEntry {
+    pub id: String,
+    pub name: String,
+    pub version: Option<String>,
+    pub description: Option<String>,
+    pub publisher: Option<String>,
+    pub install_location: Option<PathBuf>,
+    pub icon_path: Option<PathBuf>,
+    pub categories: Vec<String>,
+    pub launch: Option<LaunchCommand>,
+}
+
+pub struct LaunchCommand {
+    pub executable: PathBuf,
+    pub args: Vec<LaunchArg>,
+    pub requires_terminal: bool,
+}
+
+pub enum LaunchArg {
+    Literal(String),
+    File,
+    Files,
+    Url,
+    Urls,
+    AppName,
+    Icon,
+    DesktopFile,
+}
